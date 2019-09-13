3fed1b3594c4af359e33e1b4f0f4b08321f941c3
lopeztomasagustin <52410714+lopeztomasagustin@users.noreply.github.com>
2019-09-13 12:15:48 -0300

    Add files via upload

--- Funciones/main.c
+++ Funciones/main.c
@@ -1,38 +1,29 @@
-
+#include <stdio.h>
 #include <stdlib.h>
-#include "aritmeticas.h"
-#include "funcion.h"
-
-   /*int factorial(int);*/
-
-    int main()
-    {
-        int edad = pedirEntero("Ingrese edad: ", 1, 100);
-        int legajo = pedirEntero("Ingrese legajo: ", 0, 1000);
-        int nota = pedirEntero("Ingrese nota: ", 1, 10);
-        int documento = pedirEntero("Ingrese documento: ", 0, 10000);
-
-        printf("%d--%d--%d--%d\n\n", edad, legajo, nota, documento);
-        return 0;
-        /*int resultado;
-        resultado = factorial(5);
-        printf("El factorial es: %d", resultado);*/
-    }
-
-    int factorial(int numero)
-    {
-        int resultado;
-        if (numero == 0)
-            {
-                resultado = 1;
-            }else
-            {
-                resultado = numero * factorial(numero - 1);
-            }
-
-        return resultado;
+#include "Operaciones.h"
+#include "Input.h"
 
 
 
-    }
+
+
+void funcion(int*);
+
+
+int main()
+{
+    int numero = 100;
+   funcion(&numero);
+    printf("Numero en el main: %d", numero);
+    return 0;
+}
+
+void funcion(int* numero)
+{
+    //*numero = 5;
+    int n;
+    printf("Ingrese numero: ");
+    scanf("%d", numero);
+
+}
 
